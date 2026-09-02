b3955d273ff368aa914f5711e93c9d80c4bdea3d
andrew221121-collab <andrew221121@gmail.com>
2026-09-02 10:13:46 +0100

    Create GameConfig.lua

---
+++ Game/GameConfig.lua
@@ -0,0 +1,22 @@
+return {
+	TypeSoul = {
+		ParryInput = Enum.KeyCode.F,
+		DodgeInput = Enum.KeyCode.Space,
+		BlockInput = Enum.KeyCode.E,
+		AttackDetectionMethod = "animation",
+		VelocityThreshold = 50,
+		DetectionRange = 60,
+		ParryWindow = 0.15,
+		ParryDebounce = 0.5,
+	},
+	StrikeBorn = {
+		ParryInput = Enum.KeyCode.F,
+		DodgeInput = Enum.KeyCode.Space,
+		BlockInput = Enum.KeyCode.E,
+		AttackDetectionMethod = "velocity",
+		VelocityThreshold = 50,
+		DetectionRange = 60,
+		ParryWindow = 0.15,
+		ParryDebounce = 0.5,
+	},
+}
